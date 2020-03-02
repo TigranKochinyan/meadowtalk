c51cc6a3f25ff1f9ccdd02a333da6522fd79c681
TigranKochinyan <tigrankochinyan24@gmail.com>
2020-03-03 01:14:19 +0400

    learning handlebars, but section helper not working

--- site/meadowalk.js
+++ site/meadowalk.js
@@ -7,11 +7,11 @@ var app = express();
 var handlebars = require('express-handlebars')
 	.create({ defaultLayout: 'main',
 	helpers:{
-		section:function(name, options){
-			if(!this._sections){this._sections = {}};
-			this._sections[name] = options.fn(this);
-			return null;
-		}
+		section: function(name, options) { 
+			if (!this._sections) this._sections = {};
+			  this._sections[name] = options.fn(this); 
+			  return null;
+			}
 	}
 });
 app.engine('handlebars', handlebars.engine);
@@ -25,7 +25,50 @@ app.use(function (req, res, next) {
 	res.locals.showTests = app.get('env') != 'production' && 
 		req.query.test === '1';
 	next();
-})
+});
+
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weatherContext = getWeatherData();
+	next();
+});
+
+
+
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Портленд',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Сплошная облачность ',
+				temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Бенд',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Малооблачно',
+				temp: '55.0 F (12.8 C)',
+			},
+			{
+				name: 'Манзанита',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Небольшой дождь',
+				temp: '55.0 F (12.8 C)',
+			}
+		]
+	};
+};
+
+
+
+
+
+
+
 
 app.get('/', function(req,res) {
 	res.render('home');
