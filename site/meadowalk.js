561fac67babd82b6df150931abbe6328865f547a
TigranKochinyan <tigrankochinyan24@gmail.com>
2020-03-08 02:33:14 +0400

    doing nothing

--- site/meadowalk.js
+++ site/meadowalk.js
@@ -64,6 +64,19 @@ function getWeatherData(){
 	};
 };
 
+// var jqupload = require('jquery-file-upload-middleware' );
+// app.use('/upload', function(req, res, next){
+// 	var now = Date.now();
+// 	jqupload.fileHandler({
+// 		uploadDir: function(){
+// 			return __dirname + '/public/uploads/' + now;
+// 		},
+// 		uploadUrl: function(){
+// 			return '/uploads/' + now;
+// 		},
+// 	})(req, res, next);
+// });
+
 
 
 
